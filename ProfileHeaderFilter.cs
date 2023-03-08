70a9947f70a51909b3ea619b39370c5312968ba7
mateus-dsm <116819046+mateus-dsm@users.noreply.github.com>
2023-03-08 14:45:29 -0300

    Add files via upload

---
+++ ProfileHeaderFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ExercicioAPI1
+{
+    public class ProfileHeaderFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "Profile",
+                In = ParameterLocation.Header,
+                Required = true,
+                Example = new OpenApiString("MeuPerfil"),
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using ExercicioAPI1;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.OperationFilter<ProfileHeaderFilter>();
+});
 
 var app = builder.Build();
 
